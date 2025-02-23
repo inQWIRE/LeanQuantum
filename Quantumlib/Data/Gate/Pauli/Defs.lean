f44397a612baf8f9b186001a7ac18dc7d528ad90
Fady Adal <2masadel@gmail.com>
2025-02-23 15:08:05 -0600

    feat: use RBMap

--- Quantumlib/Data/Gate/Pauli/Defs.lean
+++ Quantumlib/Data/Gate/Pauli/Defs.lean
@@ -1,6 +1,7 @@
 import Quantumlib.Data.Complex.Basic
 import Quantumlib.Data.Matrix.Basic
 import Quantumlib.Data.Matrix.PowBitVec
+import Quantumlib.Data.Finmap.MergeWith
 
 open Kron
 
@@ -16,79 +17,144 @@ def σz : CSquare 2 :=
   !![1,  0;
      0, -1]
 
-
+/-- Unsigned Pauli -/
 @[ext]
-structure Pauli (n : ℕ) where
-  isNeg : Bool := false
+structure UPauli (n : ℕ) where
   x : BitVec n
   z : BitVec n
+deriving DecidableEq
 
-namespace Pauli
+abbrev PauliCoeff (n : ℕ) := UPauli n × ℂ
 
-def X : Pauli 1 where
+namespace UPauli
+
+instance : Ord (UPauli n) where
+  compare := compareLex (compareOn (·.x.toNat)) (compareOn (·.z.toNat))
+    
+def X : UPauli 1 where
   x := 1
   z := 0
 
-def IY : Pauli 1 where
-  isNeg := true
+def IY : UPauli 1 where
   x := 1
   z := 1
 
-def Z : Pauli 1 where
+def Z : UPauli 1 where
   x := 0
   z := 1
 
-def cons (x z : Bool) (P : Pauli n) : Pauli (n + 1) :=
+def cons (x z : Bool) (P : UPauli n) : UPauli (n + 1) :=
   {P with x := P.x.cons x, z := P.z.cons z}
 
-def tail (P : Pauli (n + 1)) : Pauli n :=
+def tail (P : UPauli (n + 1)) : UPauli n :=
   {P with x := P.x.lsbs, z := P.z.lsbs}
 
-def commutesWith (P Q : Pauli n) : Bool :=
-  ((P.x &&& Q.z).weight + (P.z &&& Q.x).weight) % 2 == 0
-
-def phaseFlipsWith (P Q : Pauli n) : Bool :=
+def phaseFlipsWith (P Q : UPauli n) : Bool :=
   (P.z &&& Q.x).weight % 2 == 1
 
-def weight (P : Pauli n) : ℕ :=
+def commutesWith (P Q : UPauli n) : Bool :=
+  P.phaseFlipsWith Q == Q.phaseFlipsWith P
+
+def weight (P : UPauli n) : ℕ :=
   P.x ||| P.z |>.weight
 
-def one : Pauli n := {x := 0, z := 0}
+def one : UPauli n := {x := 0, z := 0}
 
-instance : One (Pauli n) := ⟨one⟩
+instance : One (UPauli n) := ⟨one⟩
 
-def neg (P : Pauli n) : Pauli n := {P with isNeg := !P.isNeg}
-
-instance : Neg (Pauli n) := ⟨neg⟩
-
-def mul (P Q : Pauli n) : Pauli n := {
-  isNeg := P.isNeg ^^ Q.isNeg ^^ phaseFlipsWith P Q,
+def mul (P Q : UPauli n) : UPauli n := {
   x := P.x ^^^ Q.x,
   z := P.z ^^^ Q.z
 }
 
-instance : Mul (Pauli n) := ⟨mul⟩
+instance : Mul (UPauli n) := ⟨mul⟩
 
-noncomputable
-def toCMatrix (P : Pauli n) : CMatrix (2 ^ n) (2 ^ n) :=
-  (-1 : ℂ) ^ P.isNeg.toNat • (σx ^ᵥ P.x * σz ^ᵥ P.z)
-
-def toCMatrix' (P : Pauli n) : CMatrix (2 ^ n) (2 ^ n) :=
+def toCMatrix (P : UPauli n) : CMatrix (2 ^ n) (2 ^ n) :=
   match n with
-  | 0      => (-1 : ℂ) ^ P.isNeg.toNat • 1
+  | 0      => 1
   | n' + 1 => 
     Matrix.reindex (finCongr <| by ring) (finCongr <| by ring)
     <| Matrix.kron (a := 2) (b := 2)
-        (bitsToMat (P.x.msb, P.z.msb)) P.tail.toCMatrix'
+        (bitsToMat (P.x.msb, P.z.msb)) P.tail.toCMatrix
 where
   bitsToMat p := 
     if p.1 && p.2 then 
-      -Complex.I • σy
+      Complex.I • σy
     else if p.1 then
       σx
     else if p.2 then
       σz
     else 1
 
-end Pauli
- 
+end UPauli
+
+namespace PauliCoeff
+
+def X  : PauliCoeff 1 := (UPauli.X, 1)
+
+def IY : PauliCoeff 1 := (UPauli.IY, 1)
+
+def Z  : PauliCoeff 1 := (UPauli.Z, 1)
+
+def cons (x z : Bool) (Pc : PauliCoeff n) : PauliCoeff (n + 1) :=
+  ({Pc.1 with x := Pc.1.x.cons x, z := Pc.1.z.cons z}, Pc.2)
+
+def tail (P : UPauli (n + 1)) : UPauli n :=
+  {P with x := P.x.lsbs, z := P.z.lsbs}
+
+def neg (Pc : PauliCoeff n) : PauliCoeff n :=
+  (Pc.1, -Pc.2)
+
+instance : Neg (PauliCoeff n) := ⟨neg⟩
+
+def mul (Pc Qc : PauliCoeff n) : PauliCoeff n := 
+  (Pc.1 * Qc.1, Pc.2 * Qc.2 * (-1) ^ (Pc.1.phaseFlipsWith Qc.1).toNat)
+
+instance : Mul (PauliCoeff n) := ⟨mul⟩
+
+def smul (c : ℂ) (Pc : PauliCoeff n) := 
+  (Pc.1, c * Pc.2)
+
+instance : SMul ℂ (PauliCoeff n) := ⟨smul⟩
+
+def toCMatrix (Pc : PauliCoeff n) : CMatrix (2 ^ n) (2 ^ n) :=
+  Pc.2 • Pc.1.toCMatrix
+
+end PauliCoeff
+
+abbrev PauliMap (n : ℕ) := Lean.RBMap (UPauli n) ℂ Ord.compare
+
+namespace PauliMap
+
+open Lean (RBMap)
+
+def X  : PauliMap 1 := RBMap.ofList [PauliCoeff.X]
+def IY : PauliMap 1 := RBMap.ofList [PauliCoeff.IY]
+def Z  : PauliMap 1 := RBMap.ofList [PauliCoeff.Z]
+
+def one : PauliMap n := RBMap.ofList [1]
+
+instance : One (PauliMap n) := ⟨one⟩
+
+def add (pm qm : PauliMap n) : PauliMap n := 
+  pm.mergeBy (fun _ => (· + ·)) qm
+
+instance : Add (PauliMap n) := ⟨add⟩
+
+def smul (a : ℂ) (pm : PauliMap n) : PauliMap n :=
+  pm.filterMap (fun _ c => some (a * c))
+
+instance : SMul ℂ (PauliMap n) := ⟨smul⟩
+
+def mul (pm qm : PauliMap n) : PauliMap n :=
+  pm.fold (init := ∅) fun acc' P c₁ =>
+    qm.fold (init := acc') fun acc Q c₂ =>
+      let R := P * Q
+      let factor := (-1 : ℂ) ^ (UPauli.phaseFlipsWith P Q).toNat * c₁ * c₂
+      match acc.find? R with
+      | none   => acc.insert R factor
+      | some c => acc.insert R (c + factor)
+
+instance : Mul (PauliMap n) := ⟨mul⟩
+
+end PauliMap
--- Quantumlib/Data/Gate/Pauli/Lemmas.lean
+++ Quantumlib/Data/Gate/Pauli/Lemmas.lean
@@ -16,6 +16,12 @@ lemma σy_ne_1 : σy ≠ 1 := by
   simp [σy] at h
 
 @[simp]
+lemma I_smul_σy_ne_1 : Complex.I • σy ≠ 1 := by
+  intros h
+  apply_fun (· 0 0) at h
+  simp [σy] at h
+
+@[simp]
 lemma neg_I_smul_σy_ne_1 : -(Complex.I • σy) ≠ 1 := by
   intros h
   apply_fun (· 0 0) at h
@@ -39,6 +45,12 @@ lemma σx_ne_σy : σx ≠ σy := by
   simp at h
 
 @[simp]
+lemma σx_ne_I_smul_σy : σx ≠ Complex.I • σy := by
+  intros h
+  apply_fun (· 1 0) at h
+  simp [σx, σy] at h
+
+@[simp]
 lemma σx_ne_neg_I_smul_σy : σx ≠ -(Complex.I • σy) := by
   intros h
   apply_fun (· 0 1) at h
@@ -57,335 +69,273 @@ lemma σy_ne_σz : σy ≠ σz := by
   simp [σy, σz] at h
 
 @[simp]
+lemma I_smul_σy_ne_σz : Complex.I • σy ≠ σz := by
+  intros h
+  apply_fun (· 0 0) at h
+  simp [σy, σz] at h
+
+@[simp]
 lemma neg_I_smul_σy_ne_σz : -(Complex.I • σy) ≠ σz := by
   intros h
   apply_fun (· 0 0) at h
   simp [σy, σz] at h
 
-
-namespace Pauli
-
-theorem cons_msb_tail (P : Pauli (n + 1)) :
-  P = cons P.x.msb P.z.msb P.tail  := by 
-    simp [cons, tail, BitVec.cons_msb_lsbs]
-
 @[simp]
-theorem neg_isNeg (P : Pauli n) :
-  (-P).isNeg = !P.isNeg := by
+theorem neg_c (P : UPauli n) :
+  (-P).c = -P.c := by
     simp [(-·), Pauli.neg]
 
 @[simp]
-theorem neg_x (P : Pauli n) :
+theorem neg_x (P : UPauli n) :
   (-P).x = P.x := by
     simp [(-·), Pauli.neg]
 
 @[simp]
-theorem neg_z (P : Pauli n) :
+theorem neg_z (P : UPauli n) :
   (-P).z = P.z := by
     simp [(-·), Pauli.neg]
+
+@[simp]
+theorem cons_c (P : UPauli n) a b :
+  (cons a b P).c = P.c := by 
+    simp [cons]
   
-@[simp]
-theorem cons_x (P : Pauli n) a b :
-  (cons a b P).x = BitVec.cons a P.x := by 
-    simp [cons]
 
-@[simp]
-theorem cons_z (P : Pauli n) a b :
-  (cons a b P).z = BitVec.cons b P.z := by 
-    simp [cons]
-
-@[simp]
-theorem cons_tail (P : Pauli n) a b :
-  (cons a b P).tail = P := by 
-    simp [cons, tail]
-
-@[simp]
-theorem cons_isNeg (P : Pauli n) a b :
-  (cons a b P).isNeg = P.isNeg := by 
-    simp [cons]
-
-@[simp]
-theorem tail_isNeg (P : Pauli (n + 1)) :
-  P.tail.isNeg = P.isNeg := by 
-    simp [tail]
-
-theorem tail_x (P : Pauli (n + 1)) :
-  P.tail.x = P.x.lsbs := by 
-    simp [tail]
-
-theorem tail_z (P : Pauli (n + 1)) :
-  P.tail.z = P.z.lsbs := by 
-    simp [tail]
-
-theorem tail_neg (P : Pauli (n + 1)) :
+theorem tail_neg (P : UPauli (n + 1)) :
   (-P).tail = -P.tail := by 
     simp [tail, (-·), neg]
 
 @[simp]
-theorem one_def : (1 : Pauli n) = {
+theorem neg_def (P : UPauli n) : -P = {P with c := -P.c} := rfl
+
+@[simp]
+theorem neg_weight (P : UPauli n) : (-P).weight = P.weight := rfl
+
+theorem neg_mul (P Q : UPauli n) : -P * Q = -(P * Q) := by
+  simp [phaseFlipsWith]
+
+@[simp]
+theorem mul_neg (P Q : UPauli n) : P * -Q = -(P * Q) := by
+  simp [phaseFlipsWith]
+
+@[simp]
+theorem neg_one_mul (P : UPauli n) : -1 * P = -P := by
+  rw [neg_mul, one_mul]
+
+@[simp]
+theorem smul_def (c : ℂ) (P : UPauli n) : c • P = {P with c := c * P.c} := rfl 
+
+@[simp]
+theorem neg_one_smul (P : UPauli n) : (-1 : ℂ) • P = -P := by
+  simp
+
+namespace UPauli
+
+theorem cons_msb_tail (P : UPauli (n + 1)) :
+  P = cons P.x.msb P.z.msb P.tail  := by 
+    simp [cons, tail, BitVec.cons_msb_lsbs]
+
+@[simp]
+theorem cons_x (P : UPauli n) a b :
+  (cons a b P).x = BitVec.cons a P.x := by 
+    simp [cons]
+
+@[simp]
+theorem cons_z (P : UPauli n) a b :
+  (cons a b P).z = BitVec.cons b P.z := by 
+    simp [cons]
+
+@[simp]
+theorem cons_tail (P : UPauli n) a b :
+  (cons a b P).tail = P := by 
+    simp [cons, tail]
+
+theorem tail_x (P : UPauli (n + 1)) :
+  P.tail.x = P.x.lsbs := by 
+    simp [tail]
+
+theorem tail_z (P : UPauli (n + 1)) :
+  P.tail.z = P.z.lsbs := by 
+    simp [tail]
+
+@[simp]
+theorem one_def : (1 : UPauli n) = {
   x := 0,
   z := 0
 } := by rfl
 
 @[simp]
-lemma one_weight : (1 : Pauli n).weight = 0 := by
+lemma one_weight : (1 : UPauli n).weight = 0 := by
   simp [weight]
 
-@[simp]
-theorem neg_def (P : Pauli n) : -P = {P with isNeg := !P.isNeg} := rfl
+theorem of_length_zero (P : UPauli 0) : P = 1 := by
+  let ⟨x, z⟩ := P
+  simp [x.eq_nil, z.eq_nil]
 
-@[simp]
-theorem neg_weight (P : Pauli n) : (-P).weight = P.weight := rfl
-
-theorem of_length_zero (P : Pauli 0) : P = 1 ∨ P = -1 := by
-  let ⟨isNeg, x, z⟩ := P
-  have hx := x.eq_nil
-  have hz := z.eq_nil
-  cases isNeg <;> simp_all
-
-theorem phaseFlipsWith_cons (P Q : Pauli n) {a b c d} :
+theorem phaseFlipsWith_cons (P Q : UPauli n) {a b c d} :
   (cons a b P).phaseFlipsWith (cons c d Q) ↔
     (b && c) ^^ P.phaseFlipsWith Q := by
       cases b <;> cases c
         <;> simp_all [phaseFlipsWith, Nat.succ_mod_two_eq_one_iff]
 
 @[simp]
-theorem mul_def (P Q : Pauli n) : P * Q = {
-  isNeg := P.isNeg ^^ Q.isNeg ^^ phaseFlipsWith P Q,
+theorem mul_def (P Q : UPauli n) : P * Q = {
   x := P.x ^^^ Q.x,
   z := P.z ^^^ Q.z
 } := by
   conv =>
     lhs
-    tactic => simp_rw [(· * ·), Mul.mul, Pauli.mul]
+    tactic => simp_rw [(· * ·), Mul.mul, UPauli.mul]
 
-theorem mul_cons (P Q : Pauli n) : 
+theorem mul_cons (P Q : UPauli n) {a b c d} : 
   cons a b P * cons c d Q =
-  let r := cons (a ^^ c) (b ^^ d) (P * Q)
-  if b && c then
-    -r
-  else 
-    r := by
-      split
-      next h =>
-        simp only [cons, mul_def, phaseFlipsWith, BitVec.cons_and_cons, h, BitVec.weight_cons_true,
-          Bool.bne_assoc, BitVec.cons_xor_cons, neg_def, mk.injEq, and_self, and_true]
-        obtain h' | h' := Nat.mod_two_eq_zero_or_one (P.z &&& Q.x).weight
-        · simp [h', Nat.succ_mod_two_eq_one_iff.mpr h']
-        · simp [h', Nat.succ_mod_two_eq_zero_iff.mpr h']
-      next h =>
-        cases b <;>
-          simp [h, cons, phaseFlipsWith]
+  cons (a ^^ c) (b ^^ d) (P * Q) := by
+    simp [cons]
 
 @[simp]
-theorem mul_one (P : Pauli n) : P * 1 = P := by
+theorem mul_one (P : UPauli n) : P * 1 = P := by
   simp [phaseFlipsWith]
 
 @[simp]
-theorem one_mul (P : Pauli n) : 1 * P = P := by
+theorem one_mul (P : UPauli n) : 1 * P = P := by
   simp [phaseFlipsWith]
 
 @[simp]
-theorem neg_mul (P Q : Pauli n) : -P * Q = -(P * Q) := by
-  simp [phaseFlipsWith]
+theorem commutesWith_comm (P Q : UPauli n) : P.commutesWith Q ↔ Q.commutesWith P := by
+  simp_rw [commutesWith, Bool.beq_comm]
 
-@[simp]
-theorem mul_neg (P Q : Pauli n) : P * -Q = -(P * Q) := by
-  simp [phaseFlipsWith]
-
-@[simp]
-theorem neg_one_mul (P : Pauli n) : -1 * P = -P := by
-  rw [neg_mul, one_mul]
-
-@[simp]
-theorem mul_neg_one (P : Pauli n) : P * -1 = -P := by
-  rw [mul_neg, mul_one]
-
-theorem commutesWith_comm (P Q : Pauli n) : P.commutesWith Q ↔ Q.commutesWith P := by
-  simp [commutesWith]
-  conv =>
-    lhs
-    rw [add_comm, BitVec.and_comm, BitVec.and_comm P.x]
-
-theorem mul_comm_of_commutesWith (P Q : Pauli n) : P.commutesWith Q → P * Q = Q * P := by
-  intros h
-  simp only [mul_def, mk.injEq, phaseFlipsWith]
-  constructor
-  rotate_right
+theorem mul_comm (P Q : UPauli n) : P * Q = Q * P := by
+  simp only [mul_def, mk.injEq]
   constructor <;> simp only [BitVec.xor_comm]
-  unfold commutesWith at h
-  simp only [beq_iff_eq] at h
-  congr 1 <;> try simp only [Bool.xor_comm]
-  rw [←Nat.even_iff, Nat.even_add'] at h
-  simp only [beq_eq_beq]
-  repeat rw [←Nat.odd_iff]
-  rw [BitVec.and_comm Q.z]
-  rwa [Iff.comm]
 
-theorem mul_anticomm_of_not_commutesWith (P Q : Pauli n) : ¬P.commutesWith Q → P * Q = -(Q * P) := by
-  intros h
-  simp only [mul_def, neg_def, mk.injEq, phaseFlipsWith]
-  constructor
-  rotate_right
-  constructor <;> simp only [BitVec.xor_comm]
-  unfold commutesWith at h
-  simp only [beq_iff_eq, Nat.mod_two_not_eq_zero] at h
-  rw [←Nat.odd_iff] at h
-  cases hParity : decide (Odd (P.z &&& Q.x).weight)
-  · simp only [decide_eq_false_iff_not,
-               Nat.not_odd_iff_even] at hParity
-    rw [Nat.odd_add] at h
-    have hOtherParity := h.mpr hParity
-    rw [Nat.even_iff] at hParity
-    rw [Nat.odd_iff] at hOtherParity
-    rw [BitVec.and_comm Q.z,
-        hParity, hOtherParity]
-    simp [Bool.xor_comm]
-  · simp only [decide_eq_true_eq] at hParity
-    rw [Nat.odd_add'] at h
-    have hOtherParity := h.mp hParity
-    rw [Nat.odd_iff] at hParity
-    rw [Nat.even_iff] at hOtherParity
-    rw [hParity, BitVec.and_comm Q.z,
-        hOtherParity]
-    simp [Bool.xor_comm]
+@[simp]
+theorem one_phaseFlipsWith : phaseFlipsWith 1 P = false := by
+  simp [phaseFlipsWith]
 
-theorem mul_self (P : Pauli n) : P * P = if P.phaseFlipsWith P then -1 else 1 := by
-  split <;> simp_all
+@[simp]
+theorem phaseFlipsWith_one : phaseFlipsWith P 1 = false := by
+  simp [phaseFlipsWith]
 
-lemma toCMatrix'_cons {n} a b (P : Pauli n) : (cons a b P).toCMatrix' = 
+@[simp]
+theorem mul_self (P : UPauli n) : P * P = 1 := by
+  simp
+
+lemma toCMatrix_cons {n} a b (P : UPauli n) : (cons a b P).toCMatrix = 
     (Matrix.reindex (finCongr <| by ring) (finCongr <| by ring)
     <| Matrix.kron (a := 2) (b := 2) 
-        (toCMatrix'.bitsToMat (a, b)) P.toCMatrix') := by
-          simp [toCMatrix']
+        (toCMatrix.bitsToMat (a, b)) P.toCMatrix) := by
+          simp [toCMatrix]
 
-@[simp]
-theorem toCMatrix'_neg (P : Pauli n) :
-  (-P).toCMatrix' = -P.toCMatrix' := by
-    induction n with
-    | zero =>
-      obtain h | h := of_length_zero P
-        <;> simp_all [toCMatrix']
-    | succ n' ih => 
-      conv_lhs =>
-        rw [cons_msb_tail P]
-        simp only [toCMatrix', cons_tail, tail_neg, ih]
-      simp [Matrix.kron_neg, Matrix.submatrix_neg, toCMatrix']
-
-theorem toCMatrix'_eq_toCMatrix (P : Pauli n) : 
-  P.toCMatrix' = P.toCMatrix := by
-    induction n with
-    | zero =>
-      obtain hP | hP := of_length_zero P
-        <;> simp_all [toCMatrix', toCMatrix]
-
-    | succ n' ih =>
-      rw [cons_msb_tail P, toCMatrix'_cons, ih]
-      simp only [
-        toCMatrix, Matrix.reindex_apply,
-        cons_isNeg, cons_x, CMatrix.powBitVec_cons, cons_z,
-        Matrix.submatrix_mul_equiv, ←Matrix.mul_kron_mul,
-        toCMatrix'.bitsToMat
-      ]
-      cases P.x.msb <;> cases P.z.msb
-        <;> simp [Matrix.submatrix_neg,
-                  Matrix.submatrix_smul] 
-
-theorem toCMatrix'_bitsToMat_injective :
-  Function.Injective (toCMatrix'.bitsToMat) := by
+theorem toCMatrix_bitsToMat_injective :
+  Function.Injective (toCMatrix.bitsToMat) := by
     simp only [Function.Injective]
     intro ⟨ax, az⟩ ⟨bx, bz⟩ h
     cases ax <;> cases az <;> cases bx <;> cases bz
-      <;> simp_all [toCMatrix'.bitsToMat]
+      <;> simp_all [toCMatrix.bitsToMat]
       <;> first | simp_all | (symm at h; try simp_all)
 
-lemma toCMatrix'_bitsToMat_mul :
-  toCMatrix'.bitsToMat (a, b) * toCMatrix'.bitsToMat (c, d)
-  = let r := toCMatrix'.bitsToMat (a ^^ c, b ^^ d)
-    if b && c then -r else r := by
+lemma toCMatrix_bitsToMat_mul {a b c d} :
+  toCMatrix.bitsToMat (a, b) * toCMatrix.bitsToMat (c, d)
+  = let r := toCMatrix.bitsToMat (a ^^ c, b ^^ d)
+    if b && c then r else -r := by
       cases a <;> cases b <;> cases c <;> cases d
-        <;> simp [toCMatrix'.bitsToMat, smul_smul]
-
-theorem toCMatrix'_injective {n} :
-  Function.Injective (toCMatrix' (n := n)) := by
-    simp only [Function.Injective]
-    intros P Q h
-    induction n with
-    | zero =>
-      obtain hP | hP := of_length_zero P
-        <;> obtain hQ | hQ := of_length_zero Q
-        <;> simp_all [toCMatrix'] 
-        <;> apply_fun (· 0 0) at h
-        <;> rw [Complex.ext_iff] at h
-        <;> simp_all
-        <;> linarith
-    | succ n' ih =>
-      rw [cons_msb_tail P, cons_msb_tail Q] at h ⊢
-      simp only [toCMatrix'_cons] at h
-      admit
+        <;> simp [toCMatrix.bitsToMat, smul_smul]
 
 
-theorem mul_toCMatrix'_eq_toCMatrix'_mul_toCMatrix' (P Q : Pauli n) :
-  (P * Q).toCMatrix' = P.toCMatrix' * Q.toCMatrix' := by
+theorem mul_toCMatrix_eq_toCMatrix_mul_toCMatrix (P Q : UPauli n) :
+  (P * Q).toCMatrix = P.toCMatrix * Q.toCMatrix := by
     induction n with
     | zero => 
-      obtain hP | hP := of_length_zero P
-        <;> obtain hQ | hQ := of_length_zero Q
-        <;> simp_all [toCMatrix', phaseFlipsWith]
+      simp [toCMatrix]
     | succ n' ih =>
       conv_rhs =>
         rw [
-          Pauli.cons_msb_tail P, Pauli.cons_msb_tail Q,
-          toCMatrix'_cons, toCMatrix'_cons,
+          UPauli.cons_msb_tail P, UPauli.cons_msb_tail Q,
+          toCMatrix_cons, toCMatrix_cons,
           Matrix.reindex_apply, finCongr_symm,
           Matrix.reindex_apply, finCongr_symm,
           Matrix.submatrix_mul_equiv, ←Matrix.mul_kron_mul,
-          toCMatrix'_bitsToMat_mul
+          toCMatrix_bitsToMat_mul
         ]
       conv_lhs =>
         rw [
-          Pauli.cons_msb_tail P, Pauli.cons_msb_tail Q,
+          UPauli.cons_msb_tail P, UPauli.cons_msb_tail Q,
           mul_cons
         ]
+      rw [
+        toCMatrix_cons, ih P.tail Q.tail,
+      ]
+      simp
+
       split_ifs
         <;> simp only [
-            toCMatrix'_neg, toCMatrix'_cons,
+            toCMatrix_cons,
             ih P.tail Q.tail,
             Matrix.neg_kron,
             Matrix.submatrix_neg,
           ]
         <;> simp
 
-theorem mul_assoc (P Q R : Pauli n) : P * Q * R = P * (Q * R) := by
-  apply_fun toCMatrix'
-  simp only [mul_toCMatrix'_eq_toCMatrix'_mul_toCMatrix']
-  rw [←_root_.mul_assoc]
-  apply toCMatrix'_injective
-
-
+theorem mul_assoc (P Q R : UPauli n) : P * Q * R = P * (Q * R) := by
+  simp [BitVec.xor_assoc]
+  
 @[simp]
-lemma one_toCMatrix : (1 : Pauli n).toCMatrix = 1 := by
-  simp [toCMatrix]
+lemma one_toCMatrix : (1 : UPauli n).toCMatrix = 1 := by
+  induction n
+  case zero => simp [toCMatrix]
+  case succ n' ih =>
+    rw [show 1 = cons false false 1 by simp [cons], toCMatrix_cons, ih]
+    simp [toCMatrix.bitsToMat]
 
 @[simp]
 lemma X_toCMatrix : X.toCMatrix = σx := by
-  simp [X, toCMatrix, CMatrix.powBitVec]
+  simp [X, toCMatrix, toCMatrix.bitsToMat]
 
 @[simp]
 lemma IY_toCMatrix : IY.toCMatrix = Complex.I • σy := by
-  simp [IY, toCMatrix, CMatrix.powBitVec]
+  simp [IY, toCMatrix, toCMatrix.bitsToMat]
 
 @[simp]
 lemma Z_toCMatrix : Z.toCMatrix = σz := by
-  simp [Z, toCMatrix, CMatrix.powBitVec]
+  simp [Z, toCMatrix, toCMatrix.bitsToMat]
 
 example : 
-  -IY * Z = X := by
-    simp [IY, Z, X, phaseFlipsWith]
+  IY * Z = X := by
+    simp [IY, Z, X]
 
 example : 
-  IY * IY = -1 := by
-    simp [IY, phaseFlipsWith, BitVec.weight, BitVec.foldr]
+  IY * IY = 1 := by
+    rw [mul_self]
 
+end UPauli
 
-end Pauli
+namespace PauliMap
+
+open Lean (RBMap)
+
+@[simp]
+theorem one_def : (1 : PauliMap n) = RBMap.ofList [1] := rfl
+
+@[simp]
+theorem add_def (pm qm : PauliMap n) : 
+  pm + qm = pm.mergeBy (fun _ => (· + ·)) qm := rfl
+
+@[simp]
+theorem smul_def (a : ℂ) (pm : PauliMap n) : 
+  a • pm = pm.filterMap (fun _ c => some (a * c)) := rfl
+
+@[simp]
+theorem mul_def (pm qm : PauliMap n) :
+  pm * qm = pm.fold (init := ∅) fun acc' P c₁ =>
+    qm.fold (init := acc') fun acc Q c₂ =>
+      let R := P * Q
+      let factor := (-1 : ℂ) ^ (UPauli.phaseFlipsWith P Q).toNat * c₁ * c₂
+      match acc.find? R with
+      | none   => acc.insert R factor
+      | some c => acc.insert R (c + factor) := rfl
+
+@[simp]
+theorem one_mul {pm : PauliMap n} : 1 * pm = pm := by
+  sorry
+
+end PauliMap
