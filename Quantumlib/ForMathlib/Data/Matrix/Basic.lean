d7f8da18f0f332e6ba9c6623717a273f121165fb
Robert Rand <rnrand@gmail.com>
2026-04-28 12:24:11 -0500

    Added FMatrix (for Fins but not Complex)

--- Quantumlib/ForMathlib/Data/Matrix/Basic.lean
+++ Quantumlib/ForMathlib/Data/Matrix/Basic.lean
@@ -4,7 +4,13 @@ import Mathlib.LinearAlgebra.Matrix.Kronecker
 import Mathlib.LinearAlgebra.UnitaryGroup
 import Mathlib.Logic.Equiv.Fin.Basic
 
-abbrev CMatrix m n := Matrix (Fin m) (Fin n) ℂ
+-- Matrices over Fins
+abbrev FMatrix m n := Matrix (Fin m) (Fin n)
+abbrev FVector n := FMatrix n 1
+abbrev FSquare n := FMatrix n n
+
+-- Complex-valued Matrices over Fins
+abbrev CMatrix m n := FMatrix m n ℂ
 abbrev CVector n := CMatrix n 1
 abbrev CSquare n := CMatrix n n
 
@@ -13,15 +19,15 @@ namespace Matrix
 theorem conjTranspose_transpose_comm : ∀ (A : CMatrix m n),
   Aᴴᵀ = Aᵀᴴ := by intros; rfl
 
-@[simp]
 theorem pow_true [Fintype n] [DecidableEq n] [CommRing R] (M : Matrix n n R) :
     M ^ true.toNat = M := by simp
 
-@[simp]
 theorem pow_false [Fintype n] [DecidableEq n] [CommRing R] (M : Matrix n n R) :
     M ^ false.toNat = 1 := by simp
 
 end Matrix
 
-def CMatrix.Commute     (A B : CMatrix n n) : Prop := _root_.Commute A B
-def CMatrix.AntiCommute (A B : CMatrix n n) : Prop := A * B = -(B * A)
+-- Can I say A * B = B * A (or reference some typeclass) for the first one?
+-- Made maximally general (useful is another story)
+def FMatrix.Commute     [Mul α] [AddCommMonoid α] (A B : FMatrix n n α) : Prop := _root_.Commute A B
+def FMatrix.AntiCommute [Mul α] [AddCommMonoid α] [Neg α] (A B : FMatrix n n α) : Prop := A * B = -(B * A)
