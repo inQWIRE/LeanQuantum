f292e9067cc33dfc6370a4c8bdb81f02436d859d
William Spencer <wjbs@uchicago.edu>
2026-05-12 10:21:10 -0500

    Add definition of pad_matrix

--- Quantumlib/Data/Language/Defs.lean
+++ Quantumlib/Data/Language/Defs.lean
@@ -4,11 +4,41 @@ import Mathlib.LinearAlgebra.Matrix.Permutation
 import Mathlib.Data.Matrix.PEquiv
 import Mathlib.Data.Finset.Image
 import QuantumLib.ForMathlib.Data.Matrix.Basic
+import Mathlib.Data.BitVec
+import Batteries.Data.BitVec
+import Init.Data.Vector
+
+namespace Vector
+
+def listNotIndexed {n m} (v : Vector (Fin n) m) : List (Fin n) :=
+  (List.finRange n).filter (fun i => (v.contains i).not)
+
+end Vector
+
+namespace BitVec
+
+def vectorReindex {n m} (v : Vector (Fin n) m) (bv : BitVec n) : BitVec m :=
+  BitVec.ofFnLE (fun i => bv.getLsb (v.get i))
+
+def vectorNotIndexed {n m} (v : Vector (Fin n) m) (bv : BitVec n) : List Bool :=
+  v.listNotIndexed.map bv.getLsb
+
+end BitVec
 
 def NoDupVector (A : Type) (n : ℕ) := { v : Vector A n // v.toList.Nodup}
 
-def pad_matrix {n m} (v : NoDupVector (Fin n) m) [Zero R] [One R]
-  (U : Matrix (BitVec m) (BitVec m) R) : Matrix (BitVec n) (BitVec n) R := sorry
+
+
+def gen_pad_matrix {n m n' m'} (vn : Vector (Fin n') n)
+  (vm : Vector (Fin m') m) [Zero R]
+  (U : Matrix (BitVec n) (BitVec m) R) : Matrix (BitVec n') (BitVec m') R :=
+  fun v w =>
+    if v.vectorNotIndexed vn = w.vectorNotIndexed vm then
+      U (v.vectorReindex vn) (w.vectorReindex vm) else 0
+
+def pad_matrix {n m} (v : NoDupVector (Fin n) m) [Zero R]
+  (U : Matrix (BitVec m) (BitVec m) R) : Matrix (BitVec n) (BitVec n) R :=
+  gen_pad_matrix v.val v.val U
 
 inductive embeddedGate (U : ℕ -> Type u) (n : ℕ) : Type u where
   | embedGate {m} (u : U m) (bits : NoDupVector (Fin n) m) : embeddedGate U n
