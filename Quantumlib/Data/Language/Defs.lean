b4a64d4f84b3e7f4540894512fc9bef3a8847d64
William Spencer <wjbs@uchicago.edu>
2026-05-12 10:33:30 -0500

    Clean up circuit semantics

--- Quantumlib/Data/Language/Defs.lean
+++ Quantumlib/Data/Language/Defs.lean
@@ -4,6 +4,7 @@ import Mathlib.LinearAlgebra.Matrix.Permutation
 import Mathlib.Data.Matrix.PEquiv
 import Mathlib.Data.Finset.Image
 import QuantumLib.ForMathlib.Data.Matrix.Basic
+import QuantumLib.ForMathlib.Data.BitVec.Basic
 import Mathlib.Data.BitVec
 import Batteries.Data.BitVec
 import Init.Data.Vector
@@ -40,13 +41,30 @@ def pad_matrix {n m} (v : NoDupVector (Fin n) m) [Zero R]
   (U : Matrix (BitVec m) (BitVec m) R) : Matrix (BitVec n) (BitVec n) R :=
   gen_pad_matrix v.val v.val U
 
-inductive embeddedGate (U : ℕ -> Type u) (n : ℕ) : Type u where
-  | embedGate {m} (u : U m) (bits : NoDupVector (Fin n) m) : embeddedGate U n
+inductive EmbeddedGate (U : ℕ -> Type u) (n : ℕ) : Type u where
+  | embedGate {m} (u : U m) (bits : NoDupVector (Fin n) m) : EmbeddedGate U n
 
-def circuit (U : ℕ -> Type u) (n : ℕ) := List (embeddedGate U n)
+namespace EmbeddedGate
 
-def embeddedGateSemantics {U : ℕ -> Type u} [Zero R] [One R]
-  (SU : forall n, U n -> Matrix (BitVec n) (BitVec n) R) {n} (G : embeddedGate U n) :
+def semantics {U : ℕ -> Type u} [Zero R]
+  (SU : forall n, U n -> Matrix (BitVec n) (BitVec n) R) {n} (G : EmbeddedGate U n) :
     Matrix (BitVec n) (BitVec n) R :=
   match G with
   | .embedGate u bits => pad_matrix bits (SU _ u)
+
+end EmbeddedGate
+
+def Circuit (U : ℕ -> Type u) (n : ℕ) := List (EmbeddedGate U n)
+
+namespace Circuit
+
+instance : OfNat (Circuit U n) 1 := ⟨[]⟩
+
+instance : Mul (Circuit U n) := ⟨List.append⟩
+
+def semantics [One R] [Mul R] [AddCommMonoid R] {U : ℕ -> Type u}
+  (SU : forall n, U n -> Matrix (BitVec n) (BitVec n) R) {n} (C : Circuit U n) :
+    Matrix (BitVec n) (BitVec n) R :=
+    (C.map (fun g => g.semantics SU)).prod
+
+end Circuit
